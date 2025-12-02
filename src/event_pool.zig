3a88daa0795989e57ef675b85791fd85a26aa608
Edward Smale <self@esma.rocks>
2025-12-02 19:41:55 +0000

    Remove threading from event pool for now

--- src/event_pool.zig
+++ src/event_pool.zig
@@ -13,55 +13,59 @@ pub fn EventPool(comptime Handler: type) type {
         client: *Client,
         handler: *Handler,
 
-        thread_pool: std.Thread.Pool,
+        // thread_pool: std.Thread.Pool,
 
         pub fn init(self: *EventPoolT, allocator: std.mem.Allocator, client: *Client, handler: *Handler) !void {
             self.* = .{
                 .allocator = allocator,
                 .client = client,
                 .handler = handler,
-                .thread_pool = undefined,
+                // .thread_pool = undefined,
             };
 
-            try self.thread_pool.init(.{
-                .allocator = allocator,
-            });
+            // try self.thread_pool.init(.{
+            //     .allocator = allocator,
+            // });
         }
 
         pub fn deinit(self: *EventPoolT) void {
-            self.thread_pool.deinit();
+            // self.thread_pool.deinit();
+            _ = self;
         }
 
-        pub fn dispatchThreadImpl(self: *EventPoolT, arena: *std.heap.ArenaAllocator, event: Client.Event) !void {
-            defer self.allocator.destroy(arena);
-            defer arena.deinit();
-            try Client.Event.dispatch(event, self.handler);
-        }
+        // pub fn dispatchThreadImpl(self: *EventPoolT, arena: *std.heap.ArenaAllocator, event: Client.Event) !void {
+        //     defer self.allocator.destroy(arena);
+        //     defer arena.deinit();
+        //     try Client.Event.dispatch(event, self.handler);
+        // }
 
-        pub fn dispatchThread(self: *EventPoolT, arena: *std.heap.ArenaAllocator, event: Client.Event) void {
-            self.dispatchThreadImpl(arena, event) catch |err| {
-                std.log.err("{s}", .{@errorName(err)});
-                if (@errorReturnTrace()) |trace| {
-                    std.debug.dumpStackTrace(trace.*);
-                }
-            };
-        }
+        // pub fn dispatchThread(self: *EventPoolT, arena: *std.heap.ArenaAllocator, event: Client.Event) void {
+        //     self.dispatchThreadImpl(arena, event) catch |err| {
+        //         std.log.err("{s}", .{@errorName(err)});
+        //         if (@errorReturnTrace()) |trace| {
+        //             std.debug.dumpStackTrace(trace.*);
+        //         }
+        //     };
+        // }
 
         pub fn start(self: *EventPoolT) !void {
+            var arena: std.heap.ArenaAllocator = .init(self.allocator);
+            defer arena.deinit();
+
             while (true) {
                 if (!self.client.connected()) break;
 
-                const arena = try self.allocator.create(std.heap.ArenaAllocator);
-                errdefer self.allocator.destroy(arena);
+                // const arena = try self.allocator.create(std.heap.ArenaAllocator);
+                // errdefer self.allocator.destroy(arena);
 
-                arena.* = .init(self.allocator);
-                errdefer arena.deinit();
+                // arena.* = .init(self.allocator);
+                // errdefer arena.deinit();
 
-                const event = self.client.receive(arena) catch |e| switch (e) {
+                const event = self.client.receive(&arena) catch |e| switch (e) {
                     error.Disconnected => {
                         log.info("Got disconnect, not resuming", .{});
-                        arena.deinit();
-                        self.allocator.destroy(arena);
+                        // arena.deinit();
+                        // self.allocator.destroy(arena);
                         return;
                     },
                     error.BrokenPipe,
@@ -78,8 +82,8 @@ pub fn EventPool(comptime Handler: type) type {
                     => {
                         if (self.client.maybe_reconnect_options) |reconnect_options| {
                             log.info("Got close, but we will reconnect and resume ({})", .{e});
-                            arena.deinit();
-                            self.allocator.destroy(arena);
+                            // arena.deinit();
+                            // self.allocator.destroy(arena);
                             try self.client.connectAndLogin(reconnect_options);
                             continue;
                         } else {
@@ -90,7 +94,9 @@ pub fn EventPool(comptime Handler: type) type {
                     else => return e,
                 };
 
-                try self.thread_pool.spawn(dispatchThread, .{ self, arena, event });
+                try Client.Event.dispatch(event, self.handler);
+
+                // try self.thread_pool.spawn(dispatchThread, .{ self, arena, event });
             }
         }
     };
